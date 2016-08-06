6f41beb6c55b400f3897e70d1773d7678c7a105f
Robin Elvedi <trivialic@posteo.net>
2016-08-06 13:56:50 +0200

    add link

--- ch01_1.7-/00_test.c
+++ ch01_1.7-/00_test.c
@@ -1,5 +1,7 @@
 /*
  * Shows the pass by reference behaviour for arrays
+ * Visualization with pythontutor
+ * http://pythontutor.com/visualize.html#code=%23include%20%3Cstdio.h%3E%0A%0Avoid%20b(char%20s%5B%5D%29%20%7B%0A%20%20s%5B0%5D%20%3D%20'B'%3B%0A%7D%0A%0Aint%20main(%29%20%7B%0A%20%20char%20word%5B2%5D%3B%0A%0A%20%20word%5B0%5D%20%3D%20'A'%3B%0A%20%20word%5B1%5D%20%3D%20'%5C0'%3B%0A%0A%20%20printf(%22%25s%5Cn%22,%20word%29%3B%0A%20%20b(word%29%3B%0A%20%20printf(%22%25s%5Cn%22,%20word%29%3B%0A%7D%0A&cumulative=false&curInstr=0&heapPrimitives=false&mode=display&origin=opt-frontend.js&py=c&rawInputLstJSON=%5B%5D&textReferences=false
  */
 #include <stdio.h>
 
