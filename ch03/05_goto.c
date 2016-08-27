7edbf46183ca30ec202f8edb875aa54fe2ade19a
Robin Elvedi <trivialic@posteo.net>
2016-08-27 21:36:24 +0200

    goto

---
+++ ch03/05_goto.c
@@ -0,0 +1,36 @@
+/*
+ * Goto Collatz
+ */
+#include <stdio.h>
+
+int main() {
+    unsigned long long n, i;
+    n = 18446744073709551615;
+    i = 0;
+    printf("%llu\n", n);
+
+    start:
+    ++i;
+
+    if(n == 1) {
+      goto finish;
+    }
+    if(n % 2 == 0) {
+        n = n / 2;
+        goto print;
+    }
+    else {
+       n = n * 3 + 1;
+       goto print;
+    }
+
+    print:
+
+    printf("%llu\n", n);
+    goto start;
+
+    finish:
+
+    printf("Reached 1 in %llu steps\n", i);
+
+}
